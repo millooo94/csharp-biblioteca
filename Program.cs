5a10bf8270e207203c283f825282c4269a478a2c
millooo94 <camillomusmeciunict@gmail.com>
2023-03-22 17:52:40 +0100

    add lend search method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,123 @@
-﻿
+﻿//var millo = new User("Camillo", "Musmeci");
+//var giuseppe = new User("Giuseppe", "Rossi");
+
+//millo.Register("ok@ok.ok", "ciao94", "095678798");
+
+
+//var divinaCommedia = new Book("Divini Commedia", "1865", "Romanzo", "5", "Alessandro Manzoni", 400);
+//var mobyDick = new Book("Moby Dick", "1865", "Romanzo", "5", "Alessandro Manzoni", 400);
+//var decameron = new Book("Decameron", "1865", "Romanzo", "5", "Alessandro Manzoni", 400);
+
+
+//library.UsersList.Add(millo);
+
+//library.DocumentsList.Add(divinaCommedia);
+//library.DocumentsList.Add(mobyDick);
+//library.DocumentsList.Add(decameron);
+
+//library.addUser(giuseppe);
+
+//library.SearchLends("Marco", "Musmeci")
+
+//foreach (Document item in library.DocumentsList)
+//{
+//    Console.WriteLine(item.Title);
+//}
+
+//foreach(User item in library.UsersList)
+//{
+//    Console.WriteLine(item.FirstName);
+//}
+
+//var ricerca1 = library.searchDocument(promessiSposi);
+//Console.WriteLine(ricerca1);
+
+
+//var millo = new User("Camillo", "Musmeci");
+//millo.Register("ok@ok.com", "fhdjdd", "74839");
+
+var library = new Library();
+var millo = new User("Camillo", "Musmeci");
+var promessiSposi = new Book("Promessi Sposi", "1865", "Romanzo", "5", "Alessandro Manzoni", 400);
+library.DocumentsList.Add(promessiSposi);
+millo.Register("ok@ok.com", "fhdjdd", "74839");
+library.addUser(millo);
+User user = null;
+Console.WriteLine("welcome to goLibrary!");
+Console.WriteLine("are you registered? (type y/n)");
+var answer1 = Console.ReadLine();
+if (answer1 == "y")
+{
+    Console.WriteLine("type your first name");
+    var firstName = Console.ReadLine();
+    Console.WriteLine("type your last name");
+    var lastName = Console.ReadLine();
+    Console.WriteLine(library.SearchUsers(firstName, lastName));
+    var userObj = Console.WriteLine(library.SearchUserObj(lastName, firstName));
+} else
+{
+    Console.WriteLine("Register Now!");
+    Console.WriteLine("type your first name!");
+    var firstName = Console.ReadLine();
+    Console.WriteLine("type your last name!");
+    var lastName = Console.ReadLine();
+    user = new User(firstName, lastName);
+    Console.WriteLine("type your email");
+    var email = Console.ReadLine();
+    Console.WriteLine("type your password");
+    var password = Console.ReadLine();
+    Console.WriteLine("type your phone number");
+    var phoneNumber = Console.ReadLine();
+    user.Register(email, password, phoneNumber);
+    library.addUser(user);
+    Console.WriteLine(library.SearchUsers(firstName, lastName));
+
+}
+
+Console.WriteLine("You want to borrow a book? (type y/n)");
+var answer2 = Console.ReadLine();
+if (answer2 == "y")
+{
+    Console.WriteLine("Search an item!");
+    Console.WriteLine("You want to search by name? (type y/n)");
+    var answer3 = Console.ReadLine();
+    if(answer3 == "y")
+    {
+        Console.WriteLine("Type a title!");
+        var title = Console.ReadLine();
+        var document = library.SearchDocumentsByTitle(title);
+
+        if (document != null)
+        {
+            Console.WriteLine($"Here's your document {document.Title}");
+            Console.WriteLine("You want to borrow it? (type y/n)");
+            var answer4 = Console.ReadLine();
+            if (answer4 == "y")
+            {
+                var lend = new Lend(user, document);
+                library.LendsList.Add(lend);
+                Console.WriteLine("The lend was carried out");
+            }
+            else
+            {
+                Console.WriteLine("See you soon!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Document not found");
+        }
+    }
+
+}
+
+
+
+
+//var millo = new User("Camillo", "Musmeci");
+//millo.Register("ok@ok.com", "fhdjdd", "74839");
+//library.addUser(millo);
+//foreach (User item in library.UsersList)
+//{
+//    Console.WriteLine(item.FirstName);
+//}
---
+++ User.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class User
+{
+    public User(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string PhoneNumber { get; set; } = "";
+
+    public bool IsRegistered = false;
+
+    public void Register(string email, string password, string phoneNumber)
+    {
+        Email = email;
+        Password = password;
+        PhoneNumber = phoneNumber;
+        IsRegistered = true;
+    }
+}
+
+public class Document
+{
+    public Document(string title, string year, string subject, string bookshelf, string author)
+    {
+        var randomNumber = new Random();
+        Code = randomNumber.Next(10, 500);
+        Title = title;
+        Year = year;
+        Subject = subject;
+        Bookshelf = bookshelf;
+        Author = author;
+    }
+
+    public int Code { get; private init; }
+    public string Title { get; set; } = "";
+    public string Year { get; set; } = "";
+    public string Subject { get; set; } = "";
+    public string Bookshelf { get; set; } = "";
+    public string Author { get; set; } = "";
+}
+
+public class Book : Document
+{
+    public Book (string title, string year, string subject, string bookshelf, string author, int pagesNumber) : base (title, year, subject, bookshelf, author)
+    {
+        PagesNumber = pagesNumber;
+    }
+
+    public int PagesNumber { get; set; }
+}
+
+public class Dvd : Document
+{
+    public Dvd(string title, string year, string subject, string bookshelf, string author, int duration) : base(title, year, subject, bookshelf, author)
+    {
+        Duration = duration;
+    }
+
+    public int Duration { get; set; }
+}
+
+public class Lend
+{
+    public Lend(User user, Document document)
+    {
+        User = user;
+        Document = document;
+        LendDate = DateTime.Now;
+        LendRestitution = LendDate.AddDays(30);
+    }
+
+    public User User { get; private set; }
+    public Document Document { get; private set; }
+    public DateTime LendDate { get; private init; }
+    public DateTime? LendRestitution { get; private init; }
+}
+
+
+
+public class Library
+{
+    public Library()
+    {
+        UsersList = new List<User>();
+        DocumentsList = new List<Document>();   
+
+    }
+    public List<User> UsersList { get; private init; }
+    public List<Document> DocumentsList { get; private init; }
+    public List<Lend> LendsList { get; private init; }
+
+    public void addUser(User user)
+    {
+        UsersList.Add(user);
+    }
+    public void addDocument(Document document) {
+
+        DocumentsList.Add(document);
+    }
+    public string SearchUsers(string firstName, string lastName)
+    {
+        var filteredUsersList = new List<User>();
+
+        foreach (var item in UsersList)
+        {
+            if (item.FirstName == firstName && item.LastName == lastName)
+            {
+                filteredUsersList.Add(item);
+            }
+        }
+
+        if (filteredUsersList.Count > 0)
+        {
+
+            return "You are logged in!";
+        }
+        else
+        {
+            return "Not Found";
+        }
+    }
+
+    public Document? SearchDocumentsByTitle(string title)
+    {
+        foreach (var item in DocumentsList)
+        {
+            if (item.Title == title)
+            {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+    public User? SearchUserObj(string firstName, string lastName)
+    {
+        foreach (var item in UsersList)
+        {
+            if (item.FirstName == firstName && item.LastName == lastName)
+            {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+    public string SearchLends(string firstName, string lastName)
+    {
+        var filteredLendsList = new List<Lend>();
+
+        foreach (var item in LendsList)
+        {
+            if (item.User.FirstName == firstName && item.User.LastName == lastName)
+            {
+                filteredLendsList.Add(item);
+            }
+        }
+
+        if (filteredLendsList.Count > 0)
+        {
+            var result = "";
+            foreach (var item in filteredLendsList)
+            {
+                result += $"Titolo: {item.Document.Title}, Autore: {item.Document.Author}, Data di prestito: {item.LendRestitution}\n";
+            }
+            return result;
+        }
+        else
+        {
+            return "Nessun risultato trovato";
+        }
+    }
+
+
+
+}
+
+
+
+
